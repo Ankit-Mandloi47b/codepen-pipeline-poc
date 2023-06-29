576ad9254195fa66a81cfde0b47c195301a678cd
ankitmandloi174 <ankitmandloi174@gmail.com>
2023-06-29 12:28:43 +0530

    fixing main.py

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ def post_data(my_data:List[UploadFile]):
     combined_data = f'<html><head><style>{css_code}</style></head><body>{html_code}</body><script>{js_code}</script></html>'
     global key
     if  not os.path.exists(str(key)):
-        os.mkdir(str(key))
+        os.mkdir(f'files/{key}')
     with open(f"files/{key}/index.html","w") as f:    
         f.write(combined_data)
     key+=1
+    return {f'http://localhost:8080/{key-1}/index.html'}
 
     # it will not work with minio as local http server only hosts current directory files
 #     if not minio_client.bucket_exists('demo'):
